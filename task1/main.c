62b11c2abe62645e5c17fc068fd376ed15dcddc8
lulalalulalalulalulahe <1005660471@qq.com>
2022-10-29 21:47:51 +0800

    Add task1

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
+int main()
+{
+	double h = 0.001;
+	double a = 1;
+	int n = 0;
+	double t;
+	double x = 0;
+	double y;
+	printf("请输入步长：");
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+	printf("请输入初始值：");
+	
+	y = a*(1-h);
+    t = a - y;
+    while(t>0.001*h){
+    	y = a*(1-h);
+    	t = a - y;
+    	x = x + h;
+    	a = y;
+    	n++;
+    	printf("x = %f  f(%d)=%f\n  ",x,n,a);
+    	
+	}
+	printf("微分方程的最终定态=%f 此时x的取值为%f",a,x);
 	return 0;
-}
-
+    
+    
+} 
